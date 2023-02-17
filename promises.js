1535d89eb6887600201542be427d30c6aec85da3
jimroxodezi <jimroxodezi@gmail.com>
2023-02-17 16:16:16 +0100

    promises

--- promises.js
+++ promises.js
@@ -1,49 +1,98 @@
-// multiple promises
-let start=Date.now()
 
-function task1(time){
-    return new Promise ((resolve,reject)=>{
-        setTimeout(() => {
-            resolve("task 1 completed")
-            console.log(`task 1 complete in ${Date.now()-start}ms`)
-        }, time);
-    })
-}
-function task2(time){
-    return new Promise ((resolve,reject)=>{
-        setTimeout(() => {
-            resolve("task 2 completed")
-            console.log(`task 2 complete in: ${Date.now()-start}ms`)
-        }, time);
-    })
-}
-function task3(time){
-    return new Promise ((resolve,reject)=>{
-        setTimeout(() => {
-            resolve("task 3 completed")
-            console.log(`task 3 complete in: ${Date.now()-start}ms`)
-        }, time);
-    })
-}
+console.log("================PROMISES=============")
 
+// // multiple promises
+// let start=Date.now()
 
-// sequential running of promises
-// task1(3000)
-// .then(()=>task2(2000))
-// .then(()=>task3(1000))
-
-//console
-//task 1 complete in 3008ms
-//task 2 complete in: 5027ms
-//task 3 complete in: 6031ms
+// function task1(time){
+//     return new Promise ((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve("task 1 completed")
+//             console.log(`task 1 complete in ${Date.now()-start}ms`)
+//         }, time);
+//     })
+// }
+// function task2(time){
+//     return new Promise ((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve("task 2 completed")
+//             console.log(`task 2 complete in: ${Date.now()-start}ms`)
+//         }, time);
+//     })
+// }
+// function task3(time){
+//     return new Promise ((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve("task 3 completed")
+//             console.log(`task 3 complete in: ${Date.now()-start}ms`)
+//         }, time);
+//     })
+// }
 
 
-// parallel running of promises.
-Promise.all([task1(3000),task2(2000),task3(1000)]).then((value)=>{
-    console.log(value)//(3)['task 1 completed',' task 2 completed',' task 3 completed']
-    });
+// // sequential running of promises
+// // task1(3000)
+// // .then(()=>task2(2000))
+// // .then(()=>task3(1000))
 
-//console
-// task 3 complete in: 1011ms
-// task 2 complete in: 2001ms
-// task 1 complete in: 3005ms
+// //console
+// //task 1 complete in 3008ms
+// //task 2 complete in: 5027ms
+// //task 3 complete in: 6031ms
+
+
+// // parallel running of promises.
+// Promise.all([task1(3000),task2(2000),task3(1000)]).then((value)=>{
+//     console.log(value)//(3)['task 1 completed',' task 2 completed',' task 3 completed']
+//     });
+
+// //console
+// // task 3 complete in: 1011ms
+// // task 2 complete in: 2001ms
+// // task 1 complete in: 3005ms
+
+
+// //promise in parallel methods
+// //CASE 1: All promises are fulfilled
+// let start = Date.now()
+// function task1(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("task 1 completed")
+//             console.log(`task 1 complete in ${Date.now() - start}ms`)
+//         }, time);
+//     })
+// }
+// function task2(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("task 2 completed")
+//             console.log(`task 2 complete in: ${Date.now() - start}ms`)
+//         }, time);
+//     })
+// }
+// function task3(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("task 3 completed")
+//             console.log(`task 3 complete in: ${Date.now() - start}ms`)
+//         }, time);
+//     })
+// }
+// // Promise.all([task1(3000), task2(2000), task3(1000)])
+// // .then((value) => console.log(value)) 
+// // .catch((error)=> console.log(error))
+
+// // Promise.allSettled([task1(3000), task2(2000), task3(1000)])
+// //     .then((value) => console.log(value)) 
+// //     .catch((error)=> console.log(error))
+
+// // Promise.any([task1(3000), task2(2000), task3(1000)])
+// //     .then((value) => console.log(value))//task 3 completed
+// //     .catch((error)=> console.log(error))
+
+// Promise.race([task1(3000), task2(2000), task3(1000)])
+//     .then((value) => console.log(value))//task 3 completed
+//     .catch((error)=> console.log(error))
+
+
