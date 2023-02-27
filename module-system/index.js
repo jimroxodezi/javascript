c28270e9f7f863f58e69c3540f52aa63ec88204a
jimroxodezi <jimroxodezi@gmail.com>
2023-02-27 10:46:51 +0100

    modules

--- module-system/index.js
+++ module-system/index.js
@@ -1,5 +1,25 @@
 
 
-const hero = require("./modcache");
+// const hero = require("./modcache");
 
-console.log(hero.name);
+// console.log(hero.name);
+// hero.name = "SuperMan";
+// console.log(hero.name);
+
+// // when we require a module, it is loaded and cached for
+// // subsequent reuse. Module caching can help with performance
+// // but should be done carefully.
+// const anotherHero = require("./modcache");
+// console.log(anotherHero.name);
+
+
+const math = require("./math");
+
+const {add, subtract} = math;
+
+// console.log(math.add(2,3));
+// console.log(math.subtract(2,3));
+
+// same as above
+console.log(add(2,3));
+console.log(subtract(2,3));
---
+++ module-system/math.js
@@ -0,0 +1,25 @@
+
+// const add = (a,b) => {
+//     return a + b;
+// }
+
+// const subtract = (a,b) => {
+//     return a - b;
+// }
+
+// // module.exports = {
+// //     add: add,
+// //     subtract: subtract
+// // }
+
+// // same as above.
+// module.exports = { add, subtract};
+
+// same as above
+exports.add = (a,b) => {
+    return a + b;
+}
+
+exports.subtract = (a,b) => {
+    return a - b;
+}
