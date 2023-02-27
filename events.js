1703e76849895b3052883cc28efe778ffc0858c2
jimroxodezi <jimroxodezi@gmail.com>
2023-02-27 08:57:01 +0100

    events grokked

--- events.js
+++ events.js
@@ -3,9 +3,26 @@ const EventEmitter = require("node:events")
 class Emitter extends EventEmitter {}
 
 const emitter = new Emitter();
+// a listener is a callback function that is executed when 
+// the corresponding event is emitted.
 
-emitter.on("foo", () => {
-    console.log("an event occured");
+
+// emitter.on("foo", () => {
+//     console.log("an event occured");
+// });
+
+emitter.on("order-pizza", (size, topping) => {
+    console.log(`Here's your ${size} pizza with ${topping} topping`);
 });
 
-emitter.emit("foo")
+emitter.on("order-pizza", (size) => {
+    if (size === "large") {
+        console.log("Serving complementary drink!");
+    }
+})
+
+// to demonstrate asynchrony
+console.log("Prepare before serving");
+
+emitter.emit("foo");
+emitter.emit("order-pizza", "large", "mushroom");
