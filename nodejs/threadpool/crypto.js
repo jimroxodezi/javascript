ea117847623d23080af508dfee62dba0dd3dacc5
jimroxodezi <jimroxodezi@gmail.com>
2023-02-27 03:19:00 +0100

    threadpool

--- nodejs/threadpool/crypto.js
+++ nodejs/threadpool/crypto.js
@@ -1,11 +1,18 @@
-const crypto = require("node:crypto");
+// node uses the thread pool for asynchronous crypto
+// computations.
 
-const MAX_CALLS = 2;
+const crypto = require("node:crypto");
+const os = require("node:os");
+
+process.env.UV_THREADPOOL_SIZE = 8;// os.cpus.length;
+const MAX_CALLS = 8;
 
 const start = Date.now();
 
 for (let i = 0; i < MAX_CALLS; i++) {
-    crypto.pbkdf2("password", "salt", 1000000, 512, "sha512", () => {
-        console.log(`Hash: ${i + 1} --> ${Date.now - start}`);
+    crypto.pbkdf2("password", "salt", 100_000, 512, "sha512", () => {
+        console.log(`Hash: ${i + 1} --> ${Date.now() - start}`);
     });
 }
+
+console.log(os.cpus().length);
