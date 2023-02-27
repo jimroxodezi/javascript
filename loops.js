6857f0957b1ec708e817965f7bf2730a240a00fd
jimroxodezi <jimroxodezi@gmail.com>
2023-02-27 16:37:32 +0100

    objects

---
+++ loops.js
@@ -0,0 +1,3 @@
+
+
+// TODO: Learn JavaScript Array methods!
--- objects.js
+++ objects.js
@@ -81,3 +81,60 @@
 // console.log(obj.__proto__.toString());
 // console.log(obj2.__proto__);
 // console.log(obj2.toString());
+
+
+
+
+// const organism = {
+//     name: "amoeba",
+//     shape: "shapeless",
+// };
+
+// const obj = Object.create(organism);
+
+// console.log(obj.__proto__);
+// // sama as above
+// console.log(Object.getPrototypeOf(obj));
+
+const a = { foo: "bar", baz: "foo"};
+
+// const b = Object.assign({}, a);
+
+// console.log(Object.getOwnPropertyNames(a));
+// console.log(Object.getOwnPropertyNames(b));
+
+// // object property keys
+// for (const k of Object.getOwnPropertyNames(a)) {
+//     console.log(k);
+// }
+
+// // sama as above
+// for (const k in a) {
+//     console.log(k);
+// }
+
+// // object property values
+// for (const v of Object.values(a)) {
+//     console.log(v);
+// }
+
+// // same as above
+// for (const v in a) {
+//     console.log(v);
+// }
+
+
+
+// // looping over key-value pairs
+// for (const [k,v] of Object.entries(a)) {
+//     console.log(k,v);
+// }
+
+// for (const k in Object.entries(a)) {
+//     console.log(a);
+// }
+
+// for (const k of Object.entries(a)) console.log(a);
+
+
+
