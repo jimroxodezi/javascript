66aa423316091f894bfc4b4a0c24001991e1eeff
jimroxodezi <jimroxodezi@gmail.com>
2023-03-13 16:02:28 +0100

    node

--- node.js
+++ node.js
@@ -1,15 +1,16 @@
+const { pipeline } = require('stream');
 
 
-setInterval(() => {
-    console.log("hello");
-}, 1000);
+// setInterval(() => {
+//     console.log("hello");
+// }, 1000);
 
 // the on method adds an event listener to 
 // the named event
-process.on("SIGINT", () => {
-    console.log("I'm leaving");
-    process.exit(0);
-});
+// process.on("SIGINT", () => {
+//     console.log("I'm leaving");
+//     process.exit(0);
+// });
 
 // process.addListener("SIGINT", ()=>{
 //     // this is an alias for process.on()
@@ -19,24 +20,28 @@ process.on("SIGINT", () => {
 // by different objects that detects them (emits, in JavaScript speak)
 // and callback functions (event listeners) are executed
 // when these events are emitted.
-const exec = require('child_process').exec;
-
-exec("pwd", (err, output) => {
-    if (err) console.error;
-    console.log(output);
-});
+// `const exec = require('child_process').exec;
+// exec("pwd", (err, output) => {
+//     if (err) console.error;
+//     console.log(output);
+// });
 
 
 const spawn = require('child_process').spawn;
 
 const cat = spawn('cat');
 
-cat.stdin.write("Hello");
+cat.stdin.write("Hello ");
 
 setTimeout(() => {
-    cat.stdin.end("World");
+    cat.stdin.end("World\n");
 }, 2000);
 
-cat.stdout.on('data', (data) => {
-    console.log(data.toString('utf-8'));
+// cat.stdout.on('data', (data) => {
+//     console.log(data.toString('utf-8'));
+// });
+
+// same as above but better.
+pipeline(cat.stdout, process.stdout, (err) => {
+    if (err) { throw new Error("couldn't pipe...");}
 });
