2bfaab8ec6b06953024377e7ebe465838d707a89
jimroxodezi <jimroxodezi@gmail.com>
2023-03-18 13:25:59 +0100

    objects

--- OOP/classes.js
+++ OOP/classes.js
@@ -1,3 +1,5 @@
+
+
 // classes are templates for creating objects
 class Rectangle {
     // a rectangle has a heigh and a width
@@ -16,6 +18,8 @@ class Rectangle {
 };
 
 class PrivatePropRectangle {
+    // class field are similar to object properties.
+    // no use of const, let or var.
     #width = 0;
     #height = 0;
 
@@ -34,12 +38,32 @@ class PrivatePropRectangle {
 };
 
 
-const rect = new Rectangle(10,20);
-console.log(rect.height);   // accessible ---> 10
-console.log(rect.area);
-console.log(rect.calcArea());
+// const rect = new Rectangle(10,20);
+// console.log(rect.height);   // accessible ---> 10
+// console.log(rect.area);
+// console.log(rect.calcArea());
 
-const privRect = new PrivatePropRectangle(10,20);
-console.log(privRect.height);   // Undefined
-console.log(privRect.area); // 200
-console.log(privRect.calcArea());   // 200
+// const privRect = new PrivatePropRectangle(10,20);
+// console.log(privRect.height);   // Undefined
+// console.log(privRect.area); // 200
+// console.log(privRect.calcArea());   // 200
+
+class StaticFieldRectangle{
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    get area() {
+        return this.width * this.height;
+    }
+
+    static addArea(rect1, rect2) {
+        return rect1.area + rect2.area;
+    }
+}
+
+const rect1 = new StaticFieldRectangle(10,30);
+const rect2 = new StaticFieldRectangle(10,20);
+console.log(rect1.addArea());   // undefined: addArea method only bound to class and not instances.
+console.log(StaticFieldRectangle.addArea(rect1, rect2));
---
+++ OOP/protochain.js
@@ -0,0 +1,66 @@
+// let animal = {
+//     eats: true,
+//     walks:() => { return `every animal walks`}
+// };
+
+// let rabbit = {
+//     jumps: true,
+//     __proto__: animal,
+// };
+
+// rabbit.__proto__ = animal;
+
+
+// function Rabbit(name) {
+//     this.name = name;
+// }
+
+// Rabbit.prototype = animal;
+
+// const rabbit = new Rabbit("White Rabbit");
+// console.log(rabbit.__proto__);
+// // console.log(rabbit.name);
+// console.log(animal.__proto__);
+// console.log(rabbit.__proto__.__proto__);
+
+// let rabbit2 = new rabbit.constructor("black Rabbit");
+// console.log(rabbit2.name);
+
+
+// let obj = {}
+// const obj2 = new Object();
+
+// console.log(obj);
+// console.log(obj.__proto__);
+// // toString() returns the string representation of an object
+// console.log(obj.__proto__.toString());
+// console.log(obj2.__proto__);
+// console.log(obj2.toString());
+
+
+let user = {
+    firstName: "Jimrox",
+    lastName: "Odezi",
+
+    set name(name) {
+        // object destructuring (structured binding)
+        [this.firstName, this.lastName] = name.split(" ");
+    },
+
+    get name() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+};
+    
+    
+let admin = {
+    __proto__: user,
+    isAdmin: true,
+};
+
+console.log(admin.name);
+
+admin.name = "Odezi Ogume";
+
+console.log(admin.name);
+console.log(user.name);
