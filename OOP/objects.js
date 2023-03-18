b7f854144f7e21ad171756de93d42f6d7c802441
jimroxodezi <jimroxodezi@gmail.com>
2023-03-18 11:53:23 +0100

    dir changes

--- OOP/objects.js
+++ OOP/objects.js
@@ -136,5 +136,14 @@ const a = { foo: "bar", baz: "foo"};
 
 // for (const k of Object.entries(a)) console.log(a);
 
+// classes are templates for creating objects
+class Rectangle {
+    // a rectangle has a heigh and a width
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
 
-
+const rect = new Rectangle(10,20);
+console.log(rect.height);
--- nodejs/node.js
+++ nodejs/node.js
@@ -1,12 +1,47 @@
-// global.queueMicrotask(() => console.log("Jimrox"));
-
-// import { clearInterval } from "timers";
+const { pipeline } = require('stream');
 
 
-// console.log("Odezi");
+// setInterval(() => {
+//     console.log("hello");
+// }, 1000);
 
-// queueMicrotask(() => console.log("is a great programmer"));
+// the on method adds an event listener to 
+// the named event
+// process.on("SIGINT", () => {
+//     console.log("I'm leaving");
+//     process.exit(0);
+// });
+
+// process.addListener("SIGINT", ()=>{
+//     // this is an alias for process.on()
+// })
+
+// events happens outside the process, but are modelled
+// by different objects that detects them (emits, in JavaScript speak)
+// and callback functions (event listeners) are executed
+// when these events are emitted.
+// `const exec = require('child_process').exec;
+// exec("pwd", (err, output) => {
+//     if (err) console.error;
+//     console.log(output);
+// });
 
 
-console.log(__dirname);
-console.log(__filename);
+const spawn = require('child_process').spawn;
+
+const cat = spawn('cat');
+
+cat.stdin.write("Hello ");
+
+setTimeout(() => {
+    cat.stdin.end("World\n");
+}, 2000);
+
+// cat.stdout.on('data', (data) => {
+//     console.log(data.toString('utf-8'));
+// });
+
+// same as above but better.
+pipeline(cat.stdout, process.stdout, (err) => {
+    if (err) { throw new Error("couldn't pipe...");}
+});
