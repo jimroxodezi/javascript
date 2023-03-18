6ce5ea1f47e3d3180c1f6f9bb4cf6ea7746671fd
jimroxodezi <jimroxodezi@gmail.com>
2023-03-18 12:37:09 +0100

    classes

--- OOP/classes.js
+++ OOP/classes.js
@@ -5,7 +5,41 @@ class Rectangle {
         this.height = height;
         this.width = width;
     }
-}
+
+    calcArea() {
+        return this.height * this.width;
+    }
+
+    get area() {
+        return this.calcArea();
+    }
+};
+
+class PrivatePropRectangle {
+    #width = 0;
+    #height = 0;
+
+    constructor(width, height) {
+        this.#height = height;
+        this.#width = width;
+    }
+
+    calcArea() {
+        return this.#width * this.#height;
+    }
+
+    get area() {
+        return this.calcArea();
+    }
+};
+
 
 const rect = new Rectangle(10,20);
-console.log(rect.height);
+console.log(rect.height);   // accessible ---> 10
+console.log(rect.area);
+console.log(rect.calcArea());
+
+const privRect = new PrivatePropRectangle(10,20);
+console.log(privRect.height);   // Undefined
+console.log(privRect.area); // 200
+console.log(privRect.calcArea());   // 200
